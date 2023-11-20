0d4c13372ddf236a37cc6c36eec2f4a7a9bc2ff0
LucStr <luca@mail.com>
2023-11-20 17:48:42 +0100

    add stable packages

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -16,7 +16,7 @@
       # Add overlays your own flake exports (from overlays and pkgs dir):
       outputs.overlays.additions
       outputs.overlays.modifications
-      outputs.overlays.unstable-packages
+      outputs.overlays.stable-packages
 
       # You can also add overlays exported from other flakes:
       # neovim-nightly-overlay.overlays.default
@@ -92,7 +92,7 @@
     };
   }; 
 
-  boot.initrd.kernelModules = [ "i915" ];
+  #boot.initrd.kernelModules = [ "i915" ];
   #boot.kernelParams = [ "i915.force_probe=a788" ];
 
   networking.hostName = "scorcher"; # Define your hostname.
