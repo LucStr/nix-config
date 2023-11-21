471ce4bb73d5a7c33e3c806982c05c9284e104f8
LucStr <luca@mail.com>
2023-11-21 16:24:18 +0100

    Reenable nvidia

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -237,11 +237,11 @@
   # Enable OpenGL
   hardware.opengl = {
     enable = true;
-  #  driSupport = true;
-  #  driSupport32Bit = true;
+    driSupport = true;
+    driSupport32Bit = true;
     extraPackages = with pkgs; [
       # trying to fix `WLR_RENDERER=vulkan sway`
-  #    vulkan-validation-layers 
+      vulkan-validation-layers 
       # https://nixos.wiki/wiki/Accelerated_Video_Playback
       intel-media-driver # LIBVA_DRIVER_NAME=iHD
       vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
@@ -262,18 +262,18 @@
   services.xserver.desktopManager.plasma5.enable = true;
 
   # Load nvidia driver for Xorg and Wayland
-  #services.xserver.videoDrivers = [ "intel" ];
+  services.xserver.videoDrivers = [ "intel" ];
 
-  #hardware.nvidia = {
+  hardware.nvidia = {
 
     # Modesetting is required.
-  #  modesetting.enable = true;
+    modesetting.enable = true;
 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-  #  powerManagement.enable = false;
+    powerManagement.enable = false;
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-  #  powerManagement.finegrained = false;
+    powerManagement.finegrained = false;
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
@@ -282,25 +282,25 @@
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
-  #  open = false;
+    open = false;
 
     # Enable the Nvidia settings menu,
 	# accessible via `nvidia-settings`.
-  #  nvidiaSettings = true;
+    nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-  #  package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
     
-  #  prime = {
-  #    offload = {
-  #      enable = true;
-  #      enableOffloadCmd = true;
-  #    };
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
       #sync.enable = true;
-  #    intelBusId = "PCI:0:2:0";
-  #    nvidiaBusId = "PCI:1:0:0";
-  #  }; 
-  #};
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    }; 
+  };
 
   #specialisation = {
   #  on-the-go.configuration = {
