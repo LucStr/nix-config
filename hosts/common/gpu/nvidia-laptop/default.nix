74cac32d1dffc4f9dd43e94f1803441d4470a637
LucStr <luca@mail.com>
2024-01-08 08:47:10 +0100

    outsource nvidia-laptop

---
+++ hosts/common/gpu/nvidia-laptop/default.nix
@@ -0,0 +1,61 @@
+{ inputs, lib, pkgs, config, ... }: {
+    nixpkgs.config.packageOverrides = pkgs: {
+        vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+    }; 
+
+    # Enable OpenGL
+    hardware.opengl = {
+        enable = true;
+        driSupport = true;
+        driSupport32Bit = true;
+        extraPackages = with pkgs; [
+        # trying to fix `WLR_RENDERER=vulkan sway`
+        vulkan-validation-layers 
+        # https://nixos.wiki/wiki/Accelerated_Video_Playback
+        intel-media-driver # LIBVA_DRIVER_NAME=iHD
+        vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+        vaapiVdpau
+        libvdpau-va-gl
+        ];
+    };
+
+    # Load nvidia driver for Xorg and Wayland
+    services.xserver.videoDrivers = [ "nvidia" ];
+
+    hardware.nvidia = {
+
+        # Modesetting is required.
+        modesetting.enable = true;
+
+        # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+        powerManagement.enable = false;
+        # Fine-grained power management. Turns off GPU when not in use.
+        # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+        powerManagement.finegrained = false;
+
+        # Use the NVidia open source kernel module (not to be confused with the
+        # independent third-party "nouveau" open source driver).
+        # Support is limited to the Turing and later architectures. Full list of 
+        # supported GPUs is at: 
+        # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+        # Only available from driver 515.43.04+
+        # Currently alpha-quality/buggy, so false is currently the recommended setting.
+        open = false;
+
+        # Enable the Nvidia settings menu,
+        # accessible via `nvidia-settings`.
+        nvidiaSettings = true;
+
+        # Optionally, you may need to select the appropriate driver version for your specific GPU.
+        package = config.boot.kernelPackages.nvidiaPackages.production;
+        
+        prime = {
+            offload = {
+                enable = true;
+                enableOffloadCmd = true;
+            };
+            intelBusId = "PCI:0:2:0";
+            nvidiaBusId = "PCI:1:0:0";
+        }; 
+    };
+}
--- hosts/scorcher/default.nix
+++ hosts/scorcher/default.nix
@@ -40,6 +40,7 @@ in
 
       ../common/global
       ../common/bluetooth
+      ../common/gpu/nvidia-laptop
     ];
 
   nixpkgs = {
@@ -311,26 +312,6 @@ in
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
  
-  nixpkgs.config.packageOverrides = pkgs: {
-    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
-  }; 
-
-  # Enable OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-    extraPackages = with pkgs; [
-      # trying to fix `WLR_RENDERER=vulkan sway`
-      vulkan-validation-layers 
-      # https://nixos.wiki/wiki/Accelerated_Video_Playback
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-      vaapiVdpau
-      libvdpau-va-gl
-    ];
-  };
-
 environment.variables.VDPAU_DRIVER = "va_gl";
 environment.variables.LIBVA_DRIVER_NAME = "nvidia";
  
@@ -345,47 +326,6 @@ environment.variables.LIBVA_DRIVER_NAME = "nvidia";
   #services.xserver.displayManager.sddm.enable = true;
   #services.xserver.windowManager.qtile.enable = true;
   #services.xserver.desktopManager.plasma5.enable = true;
-
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = [ "nvidia" ];
-
-  hardware.nvidia = {
-
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    powerManagement.enable = false;
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.production;
-    
-    prime = {
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
-      };
-      #reverseSync.enable = true;
-      intelBusId = "PCI:0:2:0";
-      nvidiaBusId = "PCI:1:0:0";
-    }; 
-  };
 
   specialisation = {
     on-the-go.configuration = {
